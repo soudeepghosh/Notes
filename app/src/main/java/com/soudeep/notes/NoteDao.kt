7a84bddbdeed2a54ebe96258d85946e0c29dfcf1
soudeepghosh <soudeepghosh20@gmail.com>
2023-07-09 01:05:25 +0530

    initial commit

---
+++ app/src/main/java/com/soudeep/notes/NoteDao.kt
@@ -0,0 +1,18 @@
+package com.soudeep.notes
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+    @Delete
+    suspend fun delete(note:Note)
+    @Query("select * from notes_table order by id ASC")
+     fun getAllNotes(): LiveData<List<Note>>
+}
